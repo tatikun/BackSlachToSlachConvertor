59d98cba28fe6074141702d76861b6e35d39025c
TTTT <40955281+tatikun@users.noreply.github.com>
2023-03-11 15:59:31 +0900

    Initial commit

---
+++ BackSlachToSlachConvertor/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BackSlachToSlachConvertor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InputTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // テキストボックスから入力を取得
+            string input = InputTextBox.Text;
+
+            // バックスラッシュをスラッシュに変換
+            string output = input.Replace("\\", "/");
+
+            // 変換結果を表示するテキストボックスに設定
+            ConvertedTextBox.Text = output;
+        }
+
+    }
+
+
+}
